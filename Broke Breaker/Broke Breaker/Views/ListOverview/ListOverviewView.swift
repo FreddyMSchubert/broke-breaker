426afad7c877797a032a69f38037933e454096de
plaguemarine <39224213+plaguemarine@users.noreply.github.com>
2026-01-28 19:02:44 +0000

    simple date picker and one week calander veiw

--- Broke Breaker/Broke Breaker/Views/ListOverview/ListOverviewView.swift
+++ Broke Breaker/Broke Breaker/Views/ListOverview/ListOverviewView.swift
@@ -1,13 +1,35 @@
 import SwiftUI
 
 struct ListOverviewView: View {
+    @State private var color: Color = .blue
+    @State private var date = Date.now
+    let colums = Array(repeating: GridItem(.flexible()), count: 7)
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
             Text("Welcome. This is the list overview page.")
                 .foregroundStyle(.secondary)
             
-            Text("This is where Calum will be putting a list of all of the incomes & expenses on a given day with the ability to select the day that's being viewed at the top and the total at the bottom.");
-
+            // The date picker
+            LabeledContent("Date") {
+                DatePicker("", selection: $date, displayedComponents: .date)
+                    .labelsHidden()
+            }
+            // The calander
+            LazyVGrid(columns: colums) {
+                let days: [Date] = (0..<7).compactMap {
+                    Calendar.current.date(byAdding: .day, value: $0, to: date)
+                }
+                ForEach(days, id: \.timeIntervalSince1970) { day in
+                    Text(day.formatted(.dateTime.day()))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.secondary)
+                        .frame(maxWidth: .infinity, minHeight: 40)
+                        .background(
+                            Circle()
+                                .foregroundStyle(color.opacity(0.3))
+                        )
+                }
+            }
             Spacer()
         }
         .padding()
@@ -15,6 +37,5 @@ struct ListOverviewView: View {
     }
 }
 
-#Preview {
-    NavigationStack { ListOverviewView() }
-}
+
+// Text("This is where Calum will be putting a list of all of the incomes & expenses on a given day with the ability to select the day that's being viewed at the top and the total at the bottom.");
