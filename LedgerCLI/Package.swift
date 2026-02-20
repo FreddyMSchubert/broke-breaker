2ef4ebe222c0f3caff8276ecba60ada5eada1d3a
FreddyMSchubert <git@frederickschubert.de>
2026-02-20 00:22:00 +0000

    fixes for running on windows

--- LedgerCLI/Package.swift
+++ LedgerCLI/Package.swift
@@ -1,16 +1,21 @@
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
-		name: "LedgerCLI",
-		dependencies: [
-			.package(path: "../SharedLedger")
-	],
-	targets: [
-		.executableTarget(
-			name: "LedgerCLI",
-			dependencies: [
-				"SharedLedger"
-			]
-		)
-	]
+    name: "LedgerCLI",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "LedgerCLI", targets: ["LedgerCLI"])
+    ],
+    dependencies: [
+        .package(path: "../SharedLedger")
+    ],
+    targets: [
+        .executableTarget(
+            name: "LedgerCLI",
+            dependencies: ["SharedLedger"]
+        )
+    ]
 )
